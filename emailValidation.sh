680febe2d19b16fc68d6035484a14e32a808760c
Akshay Wankhade <akshuwankhade12@gmail.com>
2020-04-12 16:06:54 +0530

    ADDED: Check the Email position Special character

--- emailValidation.sh
+++ emailValidation.sh
@@ -2,7 +2,7 @@
 
 #EMAIL ID VALIDATION
 read -p "Enter Email ID: " email
-PatternEmail="^[a-z]{5,}@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
+PatternEmail="^[a-z]{5,}@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}).([a-zA-Z_\+\-]{2,5}$"
 
 if [[ $email =~ $PatternEmail ]]
 then
